d78287b9d2a6d2953a2a672bdf7155388b2d911e
etikivishwas <etikivishwas47@gmail.com>
2025-12-16 09:46:06 +0530

    updated gateway ids

--- src/components/WorkerDetails.jsx
+++ src/components/WorkerDetails.jsx
@@ -57,10 +57,10 @@ const gatewayCoords = {
   // "Gateway-A4": { lat: 15.5841944444, lon: 79.825075, label: "A4" },
   // "Gateway-A3": { lat: 15.5853583333, lon: 79.8240361111, label: "A3" },
 
-  "Gateway-A1": { lat: 15.58675, lon: 79.82731, label: "A1" },
-  "Gateway-A2": { lat: 15.58625, lon: 79.82715, label: "A2" },
-  'Gateway-A3': {lat: 15.58626, lon: 79.82732, label: "A3" },
-  'Gateway-A4': {lat: 15.58674, lon: 79.82786, label: "A4" },
+  "GW_G_ZONE_G": { lat: 15.58675, lon: 79.82731, label: "A1" },
+  "GW_B_ZONE_B": { lat: 15.58625, lon: 79.82715, label: "A2" },
+  'GW_E_ZONE_E': {lat: 15.58626, lon: 79.82732, label: "A3" },
+  'GW_A_ENTRANCE': {lat: 15.58674, lon: 79.82786, label: "A4" },
 }
 const bounds = [
   [15.5871234, 79.821908],  // southwest
